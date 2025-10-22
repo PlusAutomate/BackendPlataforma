c00463331f7fea111f98e0c0c8af183545f491c4
Everton Araujo Xavier <everton.araujo2023@hotmail.com>
2025-10-21 21:59:10 -0300

    Resolvend erro de CORS

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootpassword@localhost:3306/sistema_rh'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Tom:Tom4002!@localhost:3306/sistema_rh'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
+
+    # 🔹 Permitir apenas o seu frontend atual
+    CORS(app, origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ])
+
     return app

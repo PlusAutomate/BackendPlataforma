ee48f66175ced21bbf9ab6972d9cee511da8338f
Gustavo Henrique Marchi de Oliveira <125297231+GustavoHMarchi@users.noreply.github.com>
2025-10-28 17:09:32 -0300

    Change database URI and update CORS origins
    Updated database URI and allowed additional frontend origins.

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:H921415i@localhost:3306/sistema_rh'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://backenduser:sua_senha_forte@54.88.151.87:3306/sistema_rh'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -15,7 +15,8 @@ def create_app():
     # 🔹 Permitir apenas o seu frontend atual
     CORS(app, origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500,"
+        "http://18.234.110.104:5500"
     ])
 
     return app

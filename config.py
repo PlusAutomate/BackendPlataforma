094ab57400305c60feab7d6fd374da09ba15ddef
Gustavo Henrique Marchi de Oliveira <125297231+GustavoHMarchi@users.noreply.github.com>
2025-12-07 16:56:35 -0300

    remoção cors

--- config.py
+++ config.py
@@ -12,11 +12,5 @@ def create_app():
 
     db.init_app(app)
 
-    # 🔹 Permitir apenas o seu frontend atual
-    CORS(app, origins=[
-        "http://127.0.0.1:5000",
-        "http://localhost:5000,"
-        "http://44.195.153.109:5000"
-    ])
 
     return app

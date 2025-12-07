edd1ceba9fcde924e1850168bde214cdd2d63e39
Gustavo Henrique Marchi de Oliveira <125297231+GustavoHMarchi@users.noreply.github.com>
2025-12-07 15:37:05 -0300

    novo usuario do banco

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:SenhaForte123!@23.21.188.183:3306/sistema_rh'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://backenduser:SenhaForte123!@23.21.188.183:3306/sistema_rh'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
